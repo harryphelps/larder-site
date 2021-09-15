27a201eb796d403b646d9ef730062f203b11f7de
Harry Phelps <harry.phelps@octoenergy.com>
2021-09-15 08:53:24 +0100

    Add remaining serializer fields to the create form
    First we add the serializer field names as constants in the render
    function and add their inputs. Then we add them to the state in the
    constructor function. Finally, we assign the state variables to the
    create fields in the post in handleSubmit.

--- front_larder_project/src/larder/itemform.js
+++ front_larder_project/src/larder/itemform.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class ItemForm extends Component {
+	//This is the create form
 	constructor(props) {
 		super(props);
 		this.state = {
-			item_name: " "
-		};
+			item_name: " ",
+			total_in_stock: 0,
+			last_purchased_on: " ",
+			last_used_on: " ",
+			average_shelf_life: 7}
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
@@ -18,7 +22,11 @@ class ItemForm extends Component {
 		event.preventDefault();
 		console.log(this.state.item_name);
 		axios.post("http://127.0.0.1:8000/create/",{
-			name: this.state.item_name
+			name: this.state.item_name, 
+			total_in_stock: this.state.total_in_stock,
+			last_purchased_on: this.state.last_purchased_on,
+			last_used_on: this.state.last_used_on,
+			average_shelf_life: this.state.average_shelf_life,
 		})
 		.then((response) =>{
 			console.log(response);
@@ -28,12 +36,30 @@ class ItemForm extends Component {
 		});
 	}
 	render(){
-		const {item_name } = this.state;
+		const {item_name, total_in_stock, last_purchased_on, last_used_on, average_shelf_life } = this.state;
 		return (
 			<form onSubmit={this.handleSubmit}>
 				<div>
+					<div>
+					<p>Name:</p>
 					<input type="text" name="item_name" value={item_name} onChange={this.handleChange} />
-
+					</div>
+					<div>
+					<p>Total in stock:</p>
+					<input type="number" name="total_in_stock" value={total_in_stock} onChange={this.handleChange} />
+					</div>
+					<div>
+					<p>Last purchased on:</p>
+					<input type="date" name="last_purchased_on" value={last_purchased_on} onChange={this.handleChange} />
+					</div>
+					<div>
+					<p>Last used on:</p>
+					<input type="date" name="last_used_on" value={last_used_on} onChange={this.handleChange} />
+					</div>
+					<div>
+					<p>Average shelf life:</p>
+					<input type="number" name="average_shelf_life" value={average_shelf_life} onChange={this.handleChange} />
+					</div>
 				</div>
 				<input type="submit" value="Submit" />
 			</form>
